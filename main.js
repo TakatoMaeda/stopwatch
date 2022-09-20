180fde7219f61de84a794a49cc9d883063e6d692
EC2 Default User <ec2-user@ip-172-31-23-8.us-west-1.compute.internal>
2022-09-20 12:24:36 +0000

    ミリ秒単位追加

--- main.js
+++ main.js
@@ -5,24 +5,30 @@ const RESET = document.getElementById('reset');
 
 let minutes = 0; //初期値０
 let seconds = 0; //初期値０
+let miliseconds = 0;
 
 let stopWatch = () => {
-  seconds ++; //初期値に１ずつ足していく
-  if (seconds >=60 ) {
-    minutes ++;
-    seconds = 0;
+  miliseconds ++; //初期値に１ずつ足していく
+  if (miliseconds >=10 ) {
+    seconds ++;
+    miliseconds = 0;
   }else{
   //位を２個下げて０００１とかにならないようにするためのslice
-  STOP_WATCH.innerHTML = ('00' + minutes).slice(-2) + ":" + ('00' + seconds).slice(-2) ;
+  STOP_WATCH.innerHTML = ('00' + minutes).slice(-2) + ":" + ('00' + seconds).slice(-2)  + ":" + ('0' + miliseconds).slice(-1);
   }
- 
+  if (seconds >= 60) {
+   minutes ++;
+   seconds = 0;
+  }
+    STOP_WATCH.innerHTML = ('00' + minutes).slice(-2) + ":" + ('00' + seconds).slice(-2)  + ":" + ('0' + miliseconds).slice(-1);
+
  }
 
 
 let interval;
 //クリックしたら１秒ずつ時間のカウントが始まるsetinterval()
  START.addEventListener('click',() => {
-  interval =setInterval(stopWatch,1000);
+  interval =setInterval(stopWatch,100);
   START.disabled = true;//押せる、押せない
   STOP.disabled = false;
   RESET.disabled = true;
@@ -36,7 +42,7 @@ let interval;
  })
  //リセットをクリックしたら秒数も分数も０
  RESET.addEventListener('click',() => {
-   STOP_WATCH.innerHTML = '00:00';
+   STOP_WATCH.innerHTML = '00:00:0';
    minutes = 0;
    seconds = 0;
   START.disabled = false;
